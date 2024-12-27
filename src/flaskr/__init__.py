8c4f6c0b70346cf909325e48d6f1bc37d42440c6
DjHumildeDev <choches61@gmail.com>
2024-12-27 10:55:38 +0100

    varias paginas hechas

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from . import db
+from . import auth
+from . import blog
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,8 +12,12 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-    
     db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
