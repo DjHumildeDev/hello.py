969c9efba198173dd8378f183ce57eb4becb0e55
DjHumildeDev <choches61@gmail.com>
2024-12-27 12:37:51 +0100

    se pueden dar liikes

--- src/flaskr/blog.py
+++ src/flaskr/blog.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, jsonify, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -93,4 +93,18 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
+    return redirect(url_for('blog.index'))
+
+@bp.route('/<int:id>/like', methods=['POST'])
+@login_required
+def like(id):
+    post =get_post(id)
+    db = get_db()
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    else:        
+        # Incrementa en uno el valor de la columna 'likes'
+        db.execute('UPDATE post SET likes = likes + 1 WHERE id = ?', (id,))
+        db.commit()  # Asegura que los cambios se guarden en la base de datos
+        
     return redirect(url_for('blog.index'))
